1b18ea7df1cfb5172de75bc2ad791c3017c1377e
hyuntak03 <hyuntak03.lee@gmail.com>
2018-05-23 21:51:35 +0900

    create src

---
+++ src/main/java/net/sglee/wasexam/AppController.java
@@ -0,0 +1,29 @@
+package net.sglee.wasexam;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AppController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String home() {
+        return "This is root";
+    }
+    
+    @RequestMapping(value = "/A", method = RequestMethod.GET)
+    @ResponseBody
+    public String apiA() {
+    		return "This is api A";
+    }
+    
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    @ResponseBody
+    public String user(@RequestParam("name") String name) {
+    		return "Hello " + name;
+    }
+
+}
---
+++ src/main/java/net/sglee/wasexam/Application.java
@@ -0,0 +1,16 @@
+package net.sglee.wasexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+//@ComponentScan(basePackages = {
+//		"net.sglee.wasexam"})
+@SpringBootApplication
+public class Application {
+	
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(Application.class,args);
+	}
+}
