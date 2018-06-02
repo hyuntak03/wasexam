8fd28f9fc0e2f478dc99d652aeee7165de63d93a
hyuntak03 <hyuntak03.lee@gmail.com>
2018-06-02 06:36:48 -0700

    updatre AppContoller.java

--- src/main/java/net/sglee/wasexam/AppController.java
+++ src/main/java/net/sglee/wasexam/AppController.java
@@ -1,5 +1,9 @@
 package net.sglee.wasexam;
 
+/**
+ * written by Hyuntak
+ */
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
